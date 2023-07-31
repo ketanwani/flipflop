9d70572e922ad7b7fde070f6a1f39604e938f05d
Ketan Wani <ketanwani@fb.com>
2023-07-31 11:50:42 +0800

    Adding init.py and setup.py for ffproxy library

---
+++ flipflop/ffproxy/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='flipflop-proxy',
+    version='0.1.0',
+    description='Reverse proxy library for python Django',
+    author='Ketan Wani',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here if any
+    ],
+)
